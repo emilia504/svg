6d6b76004e360598ecbc99715db189eff19e259b
student <a@example.com>
2026-04-29 18:45:17 +0200

    Lab. 9, zad. 1

---
+++ powtorzenie/src/DeathCauseStatistic.java
@@ -0,0 +1,42 @@
+public class DeathCauseStatistic {
+
+    private String icd10;
+
+    private int[] deathByAge;
+
+    public DeathCauseStatistic(String icd10, int[] deathByAge) {
+        this.icd10 = icd10;
+        this.deathByAge = deathByAge;
+    }
+
+    public void setIcd10(String icd10) {
+        this.icd10 = icd10;
+    }
+
+    public String getIcd10() {
+        return icd10;
+    }
+
+    public static DeathCauseStatistic fromCsvLine(String line) {
+
+        String[] parts = line.split(",");
+
+        String icd10 = parts[0].trim();
+
+        int[] deathByAge = new int[parts.length - 2];
+
+        for (int i = 2; i < parts.length; i++) {
+            String value = parts[i];
+
+            if ("-".equals(value) || value.isEmpty()) {
+                deathByAge[i-2] = 0;
+            } else {
+                deathByAge[i-2] = Integer.parseInt(value.trim());
+            }
+        }
+
+        return new DeathCauseStatistic(icd10, deathByAge);
+
+
+    }
+}
---
+++ powtorzenie/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String line = "A09.9          ,5,-,-,-,-,-,-,-,-,-,-,-,-,-,-,1,1,-,2,1,-";
+
+        DeathCauseStatistic stat = DeathCauseStatistic.fromCsvLine(line);
+
+        System.out.println(stat.getIcd10());
+    }
+}
