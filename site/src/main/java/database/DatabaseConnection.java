4ee799c093c2a6d1967c552254b49e7268d1dcae
Emilia <x@y.z>
2026-05-13 17:06:20 +0200

    Lab. 11, zad. 0 cz. 1

--- site/src/main/java/database/DatabaseConnection.java
+++ site/src/main/java/database/DatabaseConnection.java
@@ -1,24 +1,49 @@
 package database;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private Connection connection;
+    static private final Map<String, Connection> connections = new HashMap<>();
 
-
-    public Connection getConnection() {
-        return connection;
+    static public Connection getConnection() {
+        return getConnection("");
     }
 
-    public void connect(String path) throws SQLException {
-        connection = DriverManager.getConnection("jdbc:sqlite:" + path);
+    static public Connection getConnection(String name) {
+        return connections.get(name);
     }
 
-    public void disconnect() throws SQLException {
-        connection.close();
+    static public void connect(String filePath) {
+        connect(filePath, "");
+    }
+
+    static public void connect(String filePath, String connectionName) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + filePath);
+            connections.put(connectionName, connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static public void disconnect() {
+        disconnect("");
+    }
+
+    static public void disconnect(String connectionName) {
+        try {
+            Connection connection = connections.get(connectionName);
+            connection.close();
+            connections.remove(connectionName);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
+
